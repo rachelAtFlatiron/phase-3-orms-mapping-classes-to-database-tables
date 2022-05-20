d5f6d3787b8b20d5df4da87d184f35e1e3cb9b57
Rachel Katz <rachel.katz@flatironschool.com>
2022-05-20 11:03:31 -0400

    commit tree 3c78e69e0a77d44a23eef241bcef79a60b672913

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,39 @@
+require 'pry'
+
 class Song
+  attr_accessor :name, :album, :id
 
-  attr_accessor :name, :album
-
-  def initialize(name:, album:)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
   end
 
+  def self.create(name:, album:) 
+    song = Song.new(name: name, album: album)
+    song.save
+  end 
+
+  def self.create_table 
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save 
+    sql = <<-SQL 
+      INSERT INTO songs (name, album)
+      VALUES(?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.album)
+    #save id from db into song instance
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    self
+  end 
 end
+
